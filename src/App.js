d82e3162bbd8d0f94c21d7140f6e79b422eaa3fd
bavimane <bavimaneharishgowdru@gmail.com>
2023-01-28 17:17:10 +0530

    feat: edit option

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 export default function App() {
   const [item, setItem] = useState("");
   const [items, setItems] = useState([]);
+  const [editedValues, setEditedValues] = useState({});
 
   const handleChange = (e) => {
     setItem(e.target.value);
@@ -26,9 +27,43 @@ export default function App() {
     setItem("");
   };
 
+  const handleEdit = (id) => {
+    const itemsClone = items.map((item) => {
+      if (item.id === id) return { ...item, isEdit: true };
+      return item;
+    });
+    setItems(itemsClone);
+    const editedValuesClone = { ...editedValues };
+    editedValuesClone[id] = items.find((item) => item.id === id).value;
+    setEditedValues(editedValuesClone);
+  };
+
+  const handleEditOnChange = (e, id) => {
+    const editedValuesClone = { ...editedValues };
+    editedValuesClone[id] = e.target.value;
+    setEditedValues(editedValuesClone);
+  };
+
+  const handleEditSubmit = (id) => {
+    const itemsClone = items.map((item) => {
+      if (item.id === id)
+        return { ...item, isEdit: false, value: editedValues[id] };
+      return item;
+    });
+    setItems(itemsClone);
+  };
+
   const handleDelete = (id) => {
     const filteredItems = items.filter((item) => item.id !== id);
     setItems(filteredItems);
+  };
+
+  const handleCancel = (id) => {
+    const itemsClone = items.map((item) => {
+      if (item.id === id) return { ...item, isEdit: false };
+      return item;
+    });
+    setItems(itemsClone);
   };
 
   return (
@@ -40,9 +75,19 @@ export default function App() {
       <h2>List of Items</h2>
       <ul>
         {items.map((item) => {
-          return (
+          return item.isEdit ? (
+            <div key={item.id}>
+              <input
+                value={editedValues[item.id]}
+                onChange={(e) => handleEditOnChange(e, item.id)}
+              />
+              <button onClick={() => handleEditSubmit(item.id)}>Submit</button>
+              <button onClick={() => handleCancel(item.id)}>Cancel</button>
+            </div>
+          ) : (
             <li key={item.id}>
               {item.value}{" "}
+              <button onClick={() => handleEdit(item.id)}>Edit</button>
               <button onClick={() => handleDelete(item.id)}>Remove</button>
             </li>
           );
